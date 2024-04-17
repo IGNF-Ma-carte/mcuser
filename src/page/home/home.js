9e5c113fae55fae015c690a8c9bbd2cc7b9a09be
viglino <jean-marc.viglino@ign.fr>
2024-04-17 13:41:47 +0200

    [UPD] organizations

--- src/page/home/home.js
+++ src/page/home/home.js
@@ -1,3 +1,5 @@
+import serviceURL from 'mcutils/api/serviceURL'
+
 import homeHtml from './home-page.html';
 import './home.css';
 
@@ -5,8 +7,14 @@ import './home.css';
 const home = document.getElementById('page-accueil');
 home.innerHTML = homeHtml;
 
-home.querySelectorAll('[data-role="teaser-3"] > div').forEach((div) => {
+// Organizations
+const orgaLink = home.querySelector('[data-ref="organisations"] a')
+orgaLink.href = serviceURL.mesorganizations
+
+// Click on blocks
+home.querySelectorAll('[data-role="teaser-2"] > div').forEach((div) => {
     div.addEventListener('click', () => {
-        document.location.hash = div.dataset.ref;
+        //document.location.hash = div.dataset.ref;
+        div.querySelector('a').click()
     })
 });
